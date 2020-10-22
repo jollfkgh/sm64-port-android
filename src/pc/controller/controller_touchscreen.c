5f4b52f15071d1a4b0db477fbaf23211cabb5f35
Colinxyz <44268932+Colinxyz@users.noreply.github.com>
2020-10-22 13:39:26 -0500

    Update controller_touchscreen.c

--- src/pc/controller/controller_touchscreen.c
+++ src/pc/controller/controller_touchscreen.c
@@ -20,8 +20,8 @@
 #define LEFT_EDGE ((int)floorf(SCREEN_WIDTH_API / 2 - SCREEN_HEIGHT_API / 2 * gfx_current_dimensions.aspect_ratio))
 #define RIGHT_EDGE ((int)ceilf(SCREEN_WIDTH_API / 2 + SCREEN_HEIGHT_API / 2 * gfx_current_dimensions.aspect_ratio))
 
-#define CORRECT_TOUCH_X(x) ((x * (RIGHT_EDGE - LEFT_EDGE)) + LEFT_EDGE)
-#define CORRECT_TOUCH_Y(y) (y * SCREEN_HEIGHT_API)
+#define CORRECT_TOUCH_X(x) (x //* (RIGHT_EDGE - LEFT_EDGE)) + LEFT_EDGE//)
+#define CORRECT_TOUCH_Y(y) (y //* SCREEN_HEIGHT_API//)
 
 #define JOYSTICK_SIZE 157
 
